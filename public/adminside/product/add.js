f235234f0efe48a5a88596cefe18f44bdea09f40
TienHT <hoangtienhanu881996@gmail.com>
2025-03-04 11:25:30 +0700

    Update editor for form add product

--- public/adminside/product/add.js
+++ public/adminside/product/add.js
@@ -1,4 +1,5 @@
 $(function(){
+    // function for select2
     $(".product_tag_select2").select2({
         tags: true,
         tokenSeparators: [',', ' ']
@@ -6,5 +7,44 @@ $(function(){
     $(".product_category_select2").select2({
         placeholder: "Select a product category",
         allowClear: true
-    })
+    });
+    // function for tinymce5
+    let editor_config = {
+        path_absolute : "/",
+        selector: 'textarea.tinymce5_init_content',
+        relative_urls: false,
+        plugins: [
+        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+        "searchreplace wordcount visualblocks visualchars code fullscreen",
+        "insertdatetime media nonbreaking save table directionality",
+        "emoticons template paste textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+        file_picker_callback : function(callback, value, meta) {
+        let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        let y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+        let cmsURL = editor_config.path_absolute + 'filemanager?editor=' + meta.fieldname;
+        if (meta.filetype == 'image') {
+            cmsURL = cmsURL + "&type=Images";
+        } else {
+            cmsURL = cmsURL + "&type=Files";
+        }
+
+        tinyMCE.activeEditor.windowManager.openUrl({
+            url : cmsURL,
+            title : 'Filemanager',
+            width : x * 0.8,
+            height : y * 0.8,
+            resizable : "yes",
+            close_previous : "no",
+            onMessage: (api, message) => {
+            callback(message.content);
+            }
+        });
+        }
+    };
+
+    tinymce.init(editor_config);
+
 });
--- resources/views/admin/product/add.blade.php
+++ resources/views/admin/product/add.blade.php
@@ -17,55 +17,56 @@
     <!-- /.content-header -->
  
     <!-- Main content -->
+    <form action="{{ route('products.store') }}" method = "POST" enctype="multipart/form-data">
+    @csrf
     <div class="content">
       <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
-                <form action="{{ route('products.store') }}" method = "POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="form-group">
-                        <label>Product name</label>
-                        <input type="textbox" class="form-control" name="name" placeholder="Enter product name">
-                    </div>
-                    <div class="form-group">
-                        <label>Product price</label>
-                        <input type="textbox" class="form-control" name="price" placeholder="Enter product price">
-                    </div>
-                    <div class="form-group">
-                        <label>Product avatar image</label>
-                        <input type="file" class="form-control-file" name="feature_image_path">
-                    </div>
-                    <div class="form-group">
-                        <label>Product detail image</label>
-                        <input type="file" class="form-control-file" name="image_path[]">
-                    </div>
-                    <div class="form-group">
-                      <label>Product tag</label>
-                      <select class="form-control product_tag_select2" multiple="multiple" name="tags[]">
-                        
-                      </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Select category</label>
-                        <select class="form-control product_category_select2" name="parent_id">
-                            <option value="0">Select category</option>
-                            {!! $htmlOption !!}
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Product description</label>
-                        <textarea class="form-control" name="content" row="3"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+          <div class="col-md-6">             
+              <div class="form-group">
+                <label>Product name</label>
+                <input type="textbox" class="form-control" name="name" placeholder="Enter product name">
+              </div>
+              <div class="form-group">
+                <label>Product price</label>
+                <input type="textbox" class="form-control" name="price" placeholder="Enter product price">
+              </div>
+              <div class="form-group">
+                <label>Product avatar image</label>
+                <input type="file" class="form-control-file" name="feature_image_path">
+              </div>
+              <div class="form-group">
+                <label>Product detail image</label>
+                <input type="file" class="form-control-file" name="image_path[]">
+              </div>
+              <div class="form-group">
+                <label>Product tag</label>
+                <select class="form-control product_tag_select2" multiple="multiple" name="tags[]">  
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">Select category</label>
+                <select class="form-control product_category_select2" name="parent_id">
+                    <option value="0">Select category</option>
+                      {!! $htmlOption !!}
+                </select>
+              </div>
             </div>
-        
-        
-        </div>
+          </div>
+          <div class="col-md-12">
+            <div class="form-group">
+              <label>Product description</label>
+              <textarea class="form-control tinymce5_init_content" name="content" row="3"></textarea>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
     <!-- /.content -->
+  </form>
   </div>
   <!-- /.content-wrapper -->
  
@@ -73,5 +74,7 @@
 
   @section('js')
   <script src="{{ asset('vendors/select2/select2.min.js') }}"></script>
+  <!-- <script src="{{ asset('vendors/tinymce5/tinymce.min.js') }}"></script> -->
+  <script src="https://cdn.tiny.cloud/1/ra4r4f3igtnvr2qzcdib2ve1jnmuxqoojkqqfrrwrae40qgl/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
   <script src="{{ asset('adminside/product/add.js') }}"></script>
   @endsection
