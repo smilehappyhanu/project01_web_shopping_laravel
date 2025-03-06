eb6a28ac010a5aac2df7b0298e91dd2f41c6a087
TienHT <hoangtienhanu881996@gmail.com>
2025-03-06 20:17:45 +0700

    Update form add slider

--- app/Http/Controllers/AdminSliderController.php
+++ app/Http/Controllers/AdminSliderController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Slider;
 use Illuminate\Http\Request;
 use App\Http\Requests\SliderAddRequest;
+use App\Traits\StorageImageTrait;
+use Illuminate\Support\Facades\Log;
 
 class AdminSliderController extends Controller
 {
+    use StorageImageTrait;
+    private $slider;
+    public function __construct (Slider $slider) {
+        $this->slider = $slider;
+    }
     public function index () {
         return view('admin.slider.index');
     }
@@ -14,6 +22,20 @@ class AdminSliderController extends Controller
         return view('admin.slider.add');
     }
     public function store (SliderAddRequest $request) {
-
+        try {
+            $dataInsert = [
+                'name' => $request->slider_name,
+                'description' => $request->slider_description
+            ];
+            $dataImage = $this->storageImageUpload($request,'slider_image_path','slider');
+            if (!empty($dataImage)) {
+                $dataInsert['image_name'] = $dataImage['file_name'];
+                $dataInsert['image_path'] = $dataImage['file_path'];
+            }
+            $this->slider->create($dataInsert);
+            return redirect()->route('sliders.index');
+        } catch (Exception $exception) {
+            Log::error('Message: ' .$exception->getMessage(). 'Line: '. $exception->getLine());
+        }
     }
 }
